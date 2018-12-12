967a97faa8efd924425c82d4896cd68bed51a6e1
andylitalo <aylitalo@caltech.edu>
2018-12-11 17:34:59 -0800

    working on counting and labeling blobs--eventually want to try blob_log--but having problems with binary_closing function

--- bubble_counter.py
+++ bubble_counter.py
@@ -11,9 +11,10 @@ import VideoFunctions as VF
 import UserInputFunctions as UIF
 
 import skimage.morphology
+import skimage.measure
+import skimage.color
+from scipy import ndimage
 import cv2
-
-from tkinter import messagebox
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,7 +32,8 @@ vidFile = 'glyc_co2_1057fps_238us_7V_0200_6-5bar_141mm.mp4'
 # Processing
 # dimensions of structuring element (pixels, pixels)
 selem = skimage.morphology.disk(10)
-thresh = 127
+thresh = 127 # threshold for identifying bubbles
+minSize = 20 # minimum size of object in pixels
 
 # display
 windowName = 'Video'
@@ -75,19 +77,41 @@ for v in range(nVids):
     nFrames = 10
     offset = 1270     
     cv2.namedWindow(windowName)
+    # initialize bubble count
+    nBubbles = 0
     for f in range(offset, offset + nFrames):
         print('Now showing frame #' + str(f))
         # image subtraction
         frame = IPF.mask_image(VF.extract_frame(Vid,f),mask)
         # project frame
         proj = IPF.project_im(frame, cAve)
-        # darker image must be second or else change will be 0
+        # subtract images
         deltaIm = IPF.scale_brightness(cv2.absdiff(refProj, proj))
+        
         # threshold
         threshIm = IPF.threshold_im(deltaIm, thresh)
-#        threshIm = cv2.cvtColor(threshIm,cv2.COLOR_GRAY2RGB)
+        # smooth out thresholded image
+        # close bubble
+        # TODO: something's wrong here************8
+        closed = skimage.morphology.binary_closing(threshIm, selem=selem)
+        # fill holes
+        filled = ndimage.morphology.binary_fill_holes(closed)
+        # remove fringes
+        noFringe = skimage.morphology.binary_opening(filled, selem=selem)
+        # remove small objects
+        cleanIm = skimage.morphology.remove_small_objects(noFringe, min_size=minSize)
+        
+        # label remaining objects
+        label, nObj = skimage.measure.label(cleanIm, return_num=True)
+        # update count
+        nBubbles += nObj
+        
+        # create RGB image of labeled objects
+        labeledIm = skimage.color.label2rgb(label, cleanIm)
+        
         # display image
-        twoIms = np.concatenate((deltaIm, threshIm), axis=1)
+        cleanIm = cleanIm.astype('uint8')
+        twoIms = np.concatenate((cv2.cvtColor(threshIm,cv2.COLOR_GRAY2RGB), cv2.cvtColor(closed.astype('uint8'),cv2.COLOR_GRAY2RGB)), axis=1)
         cv2.imshow(windowName, twoIms)
         # waits for allotted number of milliseconds
         k = cv2.waitKey(waitMS)
